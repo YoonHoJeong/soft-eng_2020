31e2993a703be76676e0df402027efca6abe0178
Jeong YoonHo <uno8941@gmail.com>
2020-09-23 18:47:11 +0900

    route setting done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "components/Header";
+import Category from "routes/Category";
+import Home from "routes/Home";
+import Search from "routes/Search";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+          <Route exact path="/Category">
+            <Category />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="header__logo">logo</div>
+      </Link>
+      <Link to="/">Home</Link>
+      <Link to="/Search">Search</Link>
+      <Link to="/">Category</Link>
+    </div>
+  );
+};
+
+export default Header;
