b1a10591dcf7b74ae7c0375ae950f3ad01507b38
Jeong YoonHo <uno8941@gmail.com>
2020-12-09 23:21:09 +0900

    Updates

---
+++ precache-manifest.b09c7a7595c0815432f062cdc1f0c405.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1584a36b68d167195a35d90d27486cd",
+    "url": "/soft-eng_2020/index.html"
+  },
+  {
+    "revision": "206ab8fab032173c8ce7",
+    "url": "/soft-eng_2020/static/css/main.b093fe62.chunk.css"
+  },
+  {
+    "revision": "cebb543e1829c2de9020",
+    "url": "/soft-eng_2020/static/js/2.cbe594e8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/soft-eng_2020/static/js/2.cbe594e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206ab8fab032173c8ce7",
+    "url": "/soft-eng_2020/static/js/main.e045fd1a.chunk.js"
+  },
+  {
+    "revision": "d2ff0366f480ac500b32",
+    "url": "/soft-eng_2020/static/js/runtime-main.7c870002.js"
+  },
+  {
+    "revision": "b5c0417be6eeca3548246e0d1d5d764e",
+    "url": "/soft-eng_2020/static/media/beer.b5c0417b.jpg"
+  },
+  {
+    "revision": "7f6ffa25435813c38a83973bf1e7d9f1",
+    "url": "/soft-eng_2020/static/media/main.7f6ffa25.jpg"
+  },
+  {
+    "revision": "6ebf1ec527842dfa2b4c42c06659eb7b",
+    "url": "/soft-eng_2020/static/media/main2.6ebf1ec5.jpg"
+  },
+  {
+    "revision": "9e631dbb0073ce3bd3e8c218c1decf49",
+    "url": "/soft-eng_2020/static/media/makgeolli.9e631dbb.jpg"
+  },
+  {
+    "revision": "dd829bee1fdb66af19f467fd4e1d53c4",
+    "url": "/soft-eng_2020/static/media/soju.dd829bee.jpg"
+  },
+  {
+    "revision": "5814dc904037e6426677fc12399bd7b1",
+    "url": "/soft-eng_2020/static/media/메뉴_막걸리.5814dc90.jpg"
+  },
+  {
+    "revision": "f50fd706703a3c587c08f00548f34c11",
+    "url": "/soft-eng_2020/static/media/메뉴_맥주.f50fd706.jpg"
+  },
+  {
+    "revision": "2cdbe1f3fd7072744fbc14f59c8ccd43",
+    "url": "/soft-eng_2020/static/media/메뉴_소주.2cdbe1f3.jpg"
+  },
+  {
+    "revision": "d8fb84079b25466062ca80e4c67f824a",
+    "url": "/soft-eng_2020/static/media/처음처럼.d8fb8407.jpg"
+  }
+]);
